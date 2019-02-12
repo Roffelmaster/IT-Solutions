cc32aac013d464072ddb94fb0210042cf6fa4a20
Roffelmaster <larsroffel2@gmail.com>
2019-02-12 10:07:31 +0100

    menu voor alles aangepast
    nog geen menu in manager of css

--- project1.2/leverancier-verwijderen.php
+++ project1.2/leverancier-verwijderen.php
@@ -60,6 +60,9 @@
                <li class="nav-item">
               <a class="nav-link" href="logistiek.php">logistiek</a>
             </li>
+             <li class="nav-item">
+            <a class="nav-link" href="manager.php">manager</a>
+          </li>
             <li class="nav-item">
               <a class="nav-link" href="frm_login.php">login</a>
             </li>
