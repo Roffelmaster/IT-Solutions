538bca4f9728a25e26053b06ad47e2fdd0cdaf54
Piet Puk <p.puk@gmail.com>
2019-02-13 13:16:55 +0100

    Update mijnbestelling.php

--- project1.2/mijnbestelling.php
+++ project1.2/mijnbestelling.php
@@ -7,7 +7,7 @@ include 'menu.php';
 <!DOCTYPE html>
 <html lang="en">
 
-  
+
     <!-- Page Content -->
     <div class="container">
          <h1 class="mt-5">Bestellingen</h1><hr>
@@ -28,15 +28,15 @@ mysqli_connect_error() . " (" .
 mysqli_connect_errno() . ")" );
 }
 $id = $_SESSION["id"];
-  
+
 $query = "SELECT * FROM bestelling WHERE werknemer_idwerknemer ='$id' ;";
 
 $result = mysqli_query($db, $query) or die('Error querying
 database.');
 
-   
 
-    
+
+
 while($row = mysqli_fetch_array($result)) {
 
     echo "<tr>";
@@ -54,7 +54,53 @@ while($row = mysqli_fetch_array($result)) {
     </div>
     <!-- /.container -->
 
-   
+    <div class="container">
+        <h1 class="mt-5">Status bestelling</h1><hr>
+      <p>wijzig hier de status van een bestelling.</p>
+     <form action="mijnbestelling.php"
+          method="post"
+          enctype="multipart/form-data">
+
+
+      <p>
+
+          <label for="bestellingid">bestelling:</label><br>
+          <input type="text"
+                   placeholder="voer in bestellingid"
+                   name="bestellingid" id="bestellingid"
+                   required>
+        </p>
+           <p>
+
+             <select name="status">
+     <option value="incompleet">incompleet</option>
+    </select>
+
+
+        <button name="status-veranderen"> verander de status van bestelling</button>
+
+    </form>
+    </div>
+        <?php
+
+
+    // Initialize message variable
+    $msg = "bestelling is veranderd";
+
+    // If upload button is clicked ...
+    if (isset($_POST ['status-veranderen'])) {
+    $bestellingid = mysqli_real_escape_string($db, $_POST['bestellingid']);
+    $status = mysqli_real_escape_string($db, $_POST['status']);
+
+
+    $sql = "UPDATE bestelling SET status = '$status' WHERE idbestelling = '$bestellingid'";
+    // execute query
+    mysqli_query($db, $sql) or die('Error querying
+    database.');
+
+    }
+    ?>
+
   </body>
 
 </html>
